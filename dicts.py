3ce08af3bc057aea5879ff083044f99fdd7f9eab
FerCesio <162721896+FerCesio@users.noreply.github.com>
2024-05-31 14:22:20 -0300

    Update dicts.py

--- dicts.py
+++ dicts.py
@@ -1,48 +1,43 @@
 
-def create_inventory(items):
-    """Create a dict that tracks the amount (count) of each element on the `items` list.
-
-    :param items: list - list of items to create an inventory from.
-    :return: dict - the inventory dictionary.
-    """
-    return {}
-
-
-def add_items(inventory, items):
-    """Add or increment items in inventory using elements from the items `list`.
-
-    :param inventory: dict - dictionary of existing inventory.
-    :param items: list - list of items to update the inventory with.
-    :return: dict - the inventory updated with the new items.
-    """
-    return {}
+def create_inventory(x):
+    newdict = dict()
+    for item in x:
+        newdict[item] = x.count(item)
+    return newdict
+create_inventory(["coal", "wood", "wood", "diamond", "diamond", "diamond"])
 
 
-def decrement_items(inventory, items):
-    """Decrement items in inventory using elements from the `items` list.
-
-    :param inventory: dict - inventory dictionary.
-    :param items: list - list of items to decrement from the inventory.
-    :return: dict - updated inventory with items decremented.
-    """
-    return {}
-
-
-def remove_item(inventory, item):
-    """Remove item from inventory if it matches `item` string.
-
-    :param inventory: dict - inventory dictionary.
-    :param item: str - item to remove from the inventory.
-    :return: dict - updated inventory with item removed. Current inventory if item does not match.
-    """
-    return {}
+def add_items(x,y):
+    if x == {}:
+        return create_inventory(y)
+    for item in list(set(y)):
+        x[item] = y.count(item) + x[item] if item in x else y.count(item)
+    return x   
+add_items({"coal":1}, ["wood", "iron", "coal", "wood"])
 
 
-def list_inventory(inventory):
-    """Create a list containing all (item_name, item_count) pairs in inventory.
+def decrement_items(x,y):
+    if x == {}:
+        return create_inventory(y)
+    for item in list(set(y)):
+        x[item] = x[item] - y.count(item) if item in x else y.count(item)
+        if x[item] - y.count(item) < 0:
+            x[item] = 0
+    return x
+decrement_items({"coal":2, "wood":1, "diamond":2}, ["coal", "coal", "wood", "wood", "diamond"])
 
-    :param inventory: dict - an inventory dictionary.
-    :return: list of tuples - list of key, value pairs from the inventory dictionary.
-    """
-    return {}
+
+def remove_item(x,y):
+    if y in x:
+        del x[y]
+    return x
+remove_item({"coal":2, "wood":1, "diamond":2}, "coal")
+
+
+def list_inventory(x):
+    newlist = []
+    for key in x.keys():
+        newlist.append((key, x[key]))
+    return newlist
+list_inventory({"coal":7, "wood":11, "diamond":2, "iron":7, "silver":0})
 
